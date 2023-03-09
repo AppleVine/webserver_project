62d31e5872ebc5b14e42cb93d10ef5fa4ab5e11c
AppleVine <Jakebco99@icloud.com>
2023-03-09 20:43:55 +1000

    Fixed get a user authorization to new method.

--- src/controller/users_controller.py
+++ src/controller/users_controller.py
@@ -64,14 +64,21 @@ def get_users():
         return users_schema.dump(users)
 
 
-
 @user.get("/<int:id>")
+@jwt_required(optional=True)
 def get_user(id):
-    if check_id(id) == True or check_access_boolean(role=["lab"]) == True:
+    current_user_claims = get_jwt()
+    if current_user_claims.get('user_id') == id or current_user_claims.get('role') == "lab":
         user = User.query.get(id)
         return user_schema.dump(user)
     else:
-        raise NoAuthorizationError("You are not able to view this persons details.")
+        return {"message": "You are not authorized to view this users information."}, 403
+
+
+@user.get("/user_results/<int:id>")
+def get_user_results(id):
+    user_results = db.session.query(Result).join(User).join(Product).filter(Result.staff_id == id).all()
+    return userresults_schema.dump(user_results)
 
 
 @user.put("/<int:id>")
@@ -96,7 +103,23 @@ def update_user(id):
     return {"message": "User not found"}, 404
 
 
-@user.get("/user_results/<int:id>")
-def get_user_results(id):
-    user_results = db.session.query(Result).join(User).join(Product).filter(Result.staff_id == id).all()
-    return userresults_schema.dump(user_results)
+# @user.delete("/<int:id>")
+# @jwt_required()
+# def update_user(id):
+#     current_user_claims = get_jwt()
+#     user_id = current_user_claims.get('user_id')
+#     if user_id != id:
+#         return {"message": "You are not authorized to update this user's information",
+#                 "current_user_id": f'{user_id}',
+#                 "id": f'{id}'
+#                 }, 403
+#     else:
+#         user_fields = user_schema.load(request.json)
+#         user = User.query.filter_by(id=id).first()
+#         if user:
+#             for field in user_fields:
+#                 setattr(user, field, user_fields[field])
+#             db.session.commit()
+#             token = create_access_token(identity=user_fields["username"], additional_claims={"user_id": user_id, "role": user_fields["role"]})
+#             return { "user": user_schema.dump(user), "token": token}
+#     return {"message": "User not found"}, 404
