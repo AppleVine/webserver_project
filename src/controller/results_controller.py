52ffb16ff876357e547a2c869d95ecace01c3641
AppleVine <Jakebco99@icloud.com>
2023-03-09 21:33:22 +1000

    Added get a result with exception handling.

--- src/controller/results_controller.py
+++ src/controller/results_controller.py
@@ -2,8 +2,7 @@ from flask import Blueprint, request
 from model.user import User
 from model.product import Product
 from model.result import Result
-from schema.users_schema import user_schema, users_schema
-from schema.results_schema import results_schema
+from schema.results_schema import results_schema, result_schema
 from app import db
 from flask_jwt_extended import create_access_token, jwt_required,  get_jwt
 
@@ -17,43 +16,40 @@ def get_results():
     return results_schema.dump(results)
 
 
-@user.get("/<int:id>")
-@jwt_required(optional=True)
-def get_user(id):
-    current_user_claims = get_jwt()
-    if current_user_claims.get('user_id') == id or current_user_claims.get('role') == "lab":
-        user = User.query.get(id)
-        return user_schema.dump(user)
+@result.get("/<int:id>")
+def get_result(id):
+    result = Result.query.get(id)
+    if result:
+        return result_schema.dump(result)
     else:
-        return {"message": "You are not authorized to view this users information."}, 403
+        return {"message": "This result does not exist."}
+
+# @user.get("/user_results/<int:id>")
+# def get_user_results(id):
+#     user_results = db.session.query(Result).join(User).join(Product).filter(Result.staff_id == id).all()
+#     return userresults_schema.dump(user_results)
 
 
-@user.get("/user_results/<int:id>")
-def get_user_results(id):
-    user_results = db.session.query(Result).join(User).join(Product).filter(Result.staff_id == id).all()
-    return userresults_schema.dump(user_results)
-
-
-@user.put("/<int:id>")
-@jwt_required()
-def update_user(id):
-    current_user_claims = get_jwt()
-    user_id = current_user_claims.get('user_id')
-    if user_id != id:
-        return {"message": "You are not authorized to update this user's information",
-                "current_user_id": f'{user_id}',
-                "id": f'{id}'
-                }, 403
-    else:
-        user_fields = user_schema.load(request.json)
-        user = User.query.filter_by(id=id).first()
-        if user:
-            for field in user_fields:
-                setattr(user, field, user_fields[field])
-            db.session.commit()
-            token = create_access_token(identity=user_fields["username"], additional_claims={"user_id": user_id, "role": user_fields["role"]})
-            return { "user": user_schema.dump(user), "token": token}
-    return {"message": "User not found"}, 404
+# @user.put("/<int:id>")
+# @jwt_required()
+# def update_user(id):
+#     current_user_claims = get_jwt()
+#     user_id = current_user_claims.get('user_id')
+#     if user_id != id:
+#         return {"message": "You are not authorized to update this user's information",
+#                 "current_user_id": f'{user_id}',
+#                 "id": f'{id}'
+#                 }, 403
+#     else:
+#         user_fields = user_schema.load(request.json)
+#         user = User.query.filter_by(id=id).first()
+#         if user:
+#             for field in user_fields:
+#                 setattr(user, field, user_fields[field])
+#             db.session.commit()
+#             token = create_access_token(identity=user_fields["username"], additional_claims={"user_id": user_id, "role": user_fields["role"]})
+#             return { "user": user_schema.dump(user), "token": token}
+#     return {"message": "User not found"}, 404
 
 
 # @user.delete("/<int:id>")
